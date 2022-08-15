495c7bb08225a6bfeeb771604e457223977dc6e5
zy <1464495600@qq.com>
2022-08-15 19:42:10 +0800

    third commit

--- src/main/java/com/atguigu/GetTest.java
+++ src/main/java/com/atguigu/GetTest.java
@@ -4,5 +4,6 @@ public class GetTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git2");
     }
 }
