4131906f40819965e591cb08154d45f7b90ed064
zy <1464495600@qq.com>
2022-08-15 19:49:09 +0800

    hot-fix commit

--- src/main/java/com/atguigu/GetTest.java
+++ src/main/java/com/atguigu/GetTest.java
@@ -4,6 +4,7 @@ public class GetTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
-        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
     }
 }
