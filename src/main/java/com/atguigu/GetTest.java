4fe3fb34a56f3e3f5dd22b9653ea19808ab99abc
zy <1464495600@qq.com>
2022-08-15 20:07:49 +0800

    push commit

--- src/main/java/com/atguigu/GetTest.java
+++ src/main/java/com/atguigu/GetTest.java
@@ -8,5 +8,6 @@ public class GetTest {
         System.out.println("hello git4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
